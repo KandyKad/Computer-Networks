0360a6913707a4cc57ecb66010c58e60dd2956cc
Kunal Kanade <43581648+KandyKad@users.noreply.github.com>
2020-07-27 10:32:12 +0530

    Trail 1

---
+++ Java/Client.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.net.*;
+
+class Client
+{
+	public static void main(String args[])throws Exception
+	{
+		BufferedReader user_input = new BufferedReader(new InputStreamReader(System.in));
+		
+		DatagramSocket client_socket = new DatagramSocket(); // Datagram socket used to create the UDP socket
+		
+		InetAddress IP_add = InetAddress.getByName("localhost"); // Getting IP address of the local host
+		
+		byte in_data[]  = new byte[1024]; // Array which reserver the input datagetting from server
+		
+		
+		byte out_data[] = new byte[1024]; // Creating buffer for sending the data
+		
+		String Str = user_input.readLine();
+		
+		out_data = Str.getBytes();
+		
+		DatagramPacket Packet1 = new DatagramPacket(out_data, out_data.length, IP_add, 1234); // Creating Datagram Packets where Data can be encapsulated
+		
+		client_socket.send(Packet1); // Sending packet to server
+		
+		DatagramPacket Packet4 = new DatagramPacket(in_data, in_data.length);		
+
+		client_socket.receive(Packet4);
+		
+		String receive_str = new String(Packet4.getData()); // Get Server data in string and print it.
+		
+		System.out.println(receive_str);
+				
+		client_socket.close(); // Closing client
+		
+	}
+}
---
+++ Java/Server.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.net.*;
+
+class Server
+{
+	public static void main(String args[]) throws Exception
+	{
+		DatagramSocket server_socket = new DatagramSocket(1234);
+		
+		BufferedReader server_input = new BufferedReader(new InputStreamReader(System.in)); // Create object in server to input from keyboard
+		
+		InetAddress IP_add = InetAddress.getByName("localhost"); // Getting IP address of localhost
+		
+		byte out_data[] = new byte[1024]; //Creating Buffer for sending the data
+		
+		byte in_data[] = new byte[1024];
+		
+		while(true)
+		{
+			DatagramPacket Packet2 = new DatagramPacket(in_data, in_data.length); // Create Datagram Packet
+			
+			server_socket.receive(Packet2); //Data from the client received in the Packet
+			
+			String Str = new String(Packet2.getData()); // Using getData() method returns data containing in the datagram which is stored in array of bytes
+			
+			InetAddress IP_add1 = Packet2.getAddress(); // get the IP address of client
+			
+			int port = Packet2.getPort();
+			
+			System.out.println(Str);
+			
+			String send_str = server_input.readLine(); // Create String& read server input
+			
+			out_data = send_str.getBytes(); // To send data
+			
+			DatagramPacket Packet3 = new DatagramPacket(out_data, out_data.length, IP_add1, port); // Creating Datagram Packet which data encapsulated
+			
+			server_socket.send(Packet3); // Send Packet to Client
+			
+			
+		}
+	}
+}
