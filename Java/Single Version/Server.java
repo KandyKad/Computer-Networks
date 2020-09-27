345762001e5a42e64a46a3a3bea85cbd9061b6f3
Kunal Kanade <43581648+KandyKad@users.noreply.github.com>
2020-09-27 09:01:06 +0530

    Trail 6
    Again a virtual simulation of Go-Back-N before actual implementation, like Trail 5. Some errors still pertaining in the logic of code, however code is running perfectly well. Will rectify them, and also implement with actual file transfer in next update.

--- Java/Single Version/Server.java
+++ Java/Single Version/Server.java
@@ -2,63 +2,91 @@
  * A UDP Server
  * @author Kunal Kanade
 
- Stop and Wait ARQ(Automatic Repeat Request)
+ Go Back N ARQ(Automatic Repeat Request)
+ Again just a simulation..
  */
-
-import java.io.*;
 import java.net.*;
-import java.nio.*;
+import java.io.*;
 import java.util.*;
 
-public class UDPStopAndWaitServer
+public class Server
 {
-	private static final int BUFFER_SIZE = 1024;
-	private static final int PORT = 6789;
+	public static void main(String args[]) throws Exception
+	{
+		ServerSocket server = new ServerSocket(6262);
+		
+		System.out.println("Server established.");
+		
+		Socket client = server.accept();
+		
+		ObjectOutputStream oos = new ObjectOutputStream(client.getOutputStream());
+		
+		ObjectInputStream ois = new ObjectInputStream(client.getInputStream());
+		
+		System.out.println("Client is now connected.");
+		
+		int x = (Integer)ois.readObject();
+		int k = (Integer)ois.readObject();
+		int j = 0;
+		int i = (Integer)ois.readObject();
+		
+		boolean flag = true;
+		Random r = new Random(6);
+		
+		int mod  =r.nextInt(6);
+		
+		while(mod == 1 || mod == 0)
+			mod = r.nextInt(6);
+		
+		while(true)
+		{
+			int c = k;
+			for(int h = 0; h <= x; h++)
+			{
+				System.out.print("|"+c+"|");
+				c =( c+1) % x;
+			}
+			System.out.println();
+			System.out.println();
+			
+			if(k == j)
+			{
+				System.out.println("Frame " + k + " recieved" + "\n" + "Data: " + j);
+				j++;
+				System.out.println();
+			}
 
-	public static void main(String[] args) throws IOException 
-  {
-		// Create a server socket
-		DatagramSocket serverSocket = new DatagramSocket( PORT );
+			else
+				System.out.println("Frames recieved not in correct order" + "\n" + " Expected farme: " + j + "\n"+ " Recieved frame no :" + k);
+			
+			System.out.println();
+			if(j % mod == 0 && flag)
+			{
+				System.out.println("Error found. Acknowledgement not sent. ");
+				flag = !flag;
+				j--;
+			}
+			else if(k == j-1)
+			{
+				oos.writeObject(k);
+				System.out.println("Acknowledgement sent.");
+			}
 
-		// Set up byte arrays for sending/receiving data
-    byte[] receiveData = new byte[ BUFFER_SIZE ];
-    byte[] dataForSend = new byte[ BUFFER_SIZE ];
-
-        // Infinite loop to check for connections 
-        while(true)
-        {
-
-        	// Get the received packet
-        	DatagramPacket received = new DatagramPacket( receiveData, receiveData.length );
-          serverSocket.receive( received );
-
-          // Get the message from the packet
-          int message = ByteBuffer.wrap(received.getData( )).getInt();
-
-          Random random = new Random( );             // As simulating the reliable protocol.. So, there is only two possibility that a packet will be transfered or not transfered..
-          int chance = random.nextInt( 100 );        // So taking a randon int in 1 to 100 and if its even assume that it would be transferred else not transferred.
-
-
-          // 1 in 2 chance of responding to the message. (Very Vague assumption..)
-          if( ((chance % 2) == 0) )
-          {
-              // Get packet's IP and port
-              InetAddress IPAddress = received.getAddress();
-              int port = received.getPort();
-
-              System.out.println("FROM CLIENT: " + message + " from: " + IPAddress);
-              // Convert message to uppercase 
-              dataForSend = ByteBuffer.allocate(4).putInt( message ).array();
-
-              // Send the packet data back to the client
-              DatagramPacket packet = new DatagramPacket( dataForSend, dataForSend.length, IPAddress, port );
-              serverSocket.send( packet ); 
-          } 
-
-          else 
-          {
-              System.out.println( "Oops, packet with sequence number "+ message + " was dropped");
-          }
-        }
-  }
+			System.out.println();
+			
+			if(j % mod == 0)
+				flag = !flag;
+			
+			k = (Integer)ois.readObject();
+			
+			if(k == -1)
+				break;
+			
+			i = (Integer)ois.readObject();
+		}
+		
+		System.out.println("Client finished sending data. Exiting");
+		
+		oos.writeObject(-1);
+	}
 }
